1af4719801c35d299c296ff48cce5a37eeea8455
jasongzj <jasongzj@163.com>
2018-11-18 09:42:11 +0000

    alter BaseRequest

--- app/Http/Requests/Api/AuthorizationRequest.php
+++ app/Http/Requests/Api/AuthorizationRequest.php
@@ -2,9 +2,8 @@
 
 namespace App\Http\Requests\Api;
 
-use Illuminate\Foundation\Http\FormRequest;
 
-class AuthorizationRequest extends FormRequest
+class AuthorizationRequest extends Request
 {
     /**
      * Get the validation rules that apply to the request.
