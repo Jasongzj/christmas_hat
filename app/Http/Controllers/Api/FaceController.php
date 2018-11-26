d43548d5f4673c682b6bf44ec1ae937c4db6e27f
jasongzj <jasongzj@163.com>
2018-11-26 06:21:30 +0000

    add porn detect

--- app/Http/Controllers/Api/FaceController.php
+++ app/Http/Controllers/Api/FaceController.php
@@ -5,13 +5,32 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\Api\FaceRequest;
 use Jasongzj\LaravelQcloudImage\QcloudImage;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class FaceController extends Controller
 {
+    const PORN_PICTURE = -501;
+
     public function detect(FaceRequest $request, QcloudImage $qcloudImage)
     {
         $image['file'] = $request->file('image')->getRealPath();
+        // 先检测是否黄图
+        $picture['files'][] = $image['file'];
+        $pornResponse = $qcloudImage->pornDetect($picture);
+
+        if ($pornResponse['result_list'][0]['code'] != 0) {
+            throw new BadRequestHttpException($pornResponse['result_list'][0]['message'], null, $pornResponse['result_list'][0]['code']);
+        }
+
+        if ($pornResponse['result_list'][0]['data']['result'] == 1) {
+            throw new BadRequestHttpException('这是一张黄图', null, self::PORN_PICTURE);
+        }
+
         $response = $qcloudImage->faceDetect($image, 1);
-        return $this->response->array($response);
+        if ($response['code'] != 0) {
+            throw new BadRequestHttpException($response['message'], null, $response['code']);
+        }
+
+        return $this->response->array($response['data']);
     }
 }
